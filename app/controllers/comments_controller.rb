2f14ae97b1c4caf37bb9898d5d1c11ba585fd0b4
Sitthata <aprils96950@gmail.com>
2025-01-13 15:28:56 +0700

    Implement comments creation and form rendering for products

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,21 @@
 class CommentsController < ApplicationController
+  before_action :set_product, only: %i[:create]
   def index
+  end
+
+  def create
+    @comment = @product.comments.build(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      redirect_to @product, notice: "Comment was successfully added."
+    else
+      redirect_to @product, alert: "Failed to add comment."
+    end
+  end
+
+  def new
+    @comment = Comment.new
   end
 
   private
